40aead9658295373509166fea64912f1ec6efd99
Dileep <dileep.dunaboyina@nimbleaccounting.com>
2024-09-04 21:06:24 +0530

    changes 04-09-24

--- app/components/barChart.tsx
+++ app/components/barChart.tsx
@@ -8,7 +8,10 @@ import {
   Title,
   Tooltip,
   Legend,
+  ChartOptions,
 } from "chart.js";
+
+// Register the necessary Chart.js components
 ChartJS.register(
   CategoryScale,
   LinearScale,
@@ -17,25 +20,26 @@ ChartJS.register(
   Tooltip,
   Legend
 );
+
 const BarChart = () => {
-    const data = {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [
-          {
-            label: "Sales",
-            data: [65, 59, 80, 81, 56, 55, 40],
-            backgroundColor: "rgba(75, 192, 192, 0.6)",
-            borderColor: "rgba(75, 192, 192, 1)",
-            borderWidth: 1,
-          },
-        ],
-      };
-      // Define the options for the chart
-  const options = {
+  const data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "Sales",
+        data: [65, 59, 80, 81, 56, 55, 40],
+        backgroundColor: "rgba(75, 192, 192, 0.6)",
+        borderColor: "rgba(75, 192, 192, 1)",
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options: ChartOptions<"bar"> = {
     responsive: true,
     plugins: {
       legend: {
-        position: "top", // Position of the legend (top, bottom, left, right)
+        position: "top" as const,
       },
       title: {
         display: true,
@@ -48,7 +52,8 @@ const BarChart = () => {
       },
     },
   };
-  return <Bar data={data} options={options} />;;
+
+  return <Bar data={data} options={options} />;
 };
 
 export default BarChart;
